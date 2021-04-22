b370e29fe7a0f6fdc1db2323156c40f8358ab43c
Edwin Jorge <ed.jorge1122@gmail.com>
2021-04-21 22:22:38 -0500

    home img for @media 1366px added but NOT the contact img...

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,12 +3,18 @@ const hamburguer = document.querySelector('.menu-btn__burger')
 const nav = document.querySelector('.nav')
 const menuNav = document.querySelector('.menu-nav')
 const navItems = document.querySelectorAll('.menu-nav__item')
+const divHome = document.getElementById('home__div')
+const newDivHome = document.createElement('div')
+const divContact = document.getElementById('contact__list_id')
+const newDivContact = document.createElement('div')
 const browserWidth = window.outerWidth
 const specificScreenSize = 1366 
 
 let showMenu = false
 
 menuBtn.addEventListener('click', toggleMenu)
+divHome.addEventListener('DOMContentLoaded', responsiveHome)
+// divContact.addEventListener('DOMContentLoaded', responsiveContact)
 
 function toggleMenu() {
   if (!showMenu) {
@@ -28,8 +34,19 @@ function toggleMenu() {
   }
 }
 
-function responsiveWidth() {
+function responsiveHome() {
   if (browserWidth == specificScreenSize) {
-    
+    divHome.insertAdjacentElement('afterend', newDivHome)
+    newDivHome.classList.add('home__img')
   }
-} 
+}
+
+// function responsiveContact() {
+//   if (browserWidth == specificScreenSize) {
+//     divContact.insertAdjacentElement('afterend', newDivContact)
+//     newDivContact.classList.add('contact__img')    
+//   }
+// }
+
+responsiveHome()
+// responsiveContact()
